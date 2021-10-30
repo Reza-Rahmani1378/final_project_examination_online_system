2c39c9856100ceb59c1929fd3bbb9363a5c281fd
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-10-30 16:10:41 +0330

    add Student

---
+++ examination_online_system/src/main/java/ir/maktab/examination_online_system/models/Student.java
@@ -0,0 +1,13 @@
+package ir.maktab.examination_online_system.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = Student.TABLE_NAME)
+public class Student extends User{
+    public static final String TABLE_NAME = "student_table";
+
+
+    //
+}
