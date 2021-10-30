449f35293f8a0f6a25a65ebd18d83e03b5bef82f
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-10-30 15:38:22 +0330

    add files for begin the project

---
+++ examination_online_system/src/main/java/ir/maktab/examination_online_system/ExaminationOnlineSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.maktab.examination_online_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationOnlineSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationOnlineSystemApplication.class, args);
+	}
+
+}
---
+++ examination_online_system/src/test/java/ir/maktab/examination_online_system/ExaminationOnlineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.examination_online_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationOnlineSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
