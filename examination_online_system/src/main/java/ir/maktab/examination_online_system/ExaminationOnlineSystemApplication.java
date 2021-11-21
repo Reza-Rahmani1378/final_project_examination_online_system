157546999294c80ff2dc1fbcbb097815a67e2b23
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-11-21 06:56:25 +0330

    ExaminationOnlineSystemApplication

--- examination_online_system/src/main/java/ir/maktab/examination_online_system/ExaminationOnlineSystemApplication.java
+++ examination_online_system/src/main/java/ir/maktab/examination_online_system/ExaminationOnlineSystemApplication.java
@@ -2,12 +2,19 @@ package ir.maktab.examination_online_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExaminationOnlineSystemApplication {
+public class ExaminationOnlineSystemApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExaminationOnlineSystemApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExaminationOnlineSystemApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminationOnlineSystemApplication.class, args);
+    }
 
 }
